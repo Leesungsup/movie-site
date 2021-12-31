b7393c3194e4a9ea82d8c2f45955ed76cf7b9cd9
Leesungsup <tmdtjq0116@naver.com>
2022-01-01 02:26:15 +0900

    LandingPage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,4 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 
-export const API_URL='https://api.themoviedb.org/3/'
+export const API_URL='https://api.themoviedb.org/3/';
+export const API_KEY='c5d34d3442bbd53bdb27dd56beb8c4e8';
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
